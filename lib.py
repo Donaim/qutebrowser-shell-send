9d73df129c47383358f77dc1903df6689fbdb90e
Donaim <3Donaim@gmail.com>
2019-09-10 20:25:18 +0200

    fixes

--- lib.py
+++ lib.py
@@ -8,12 +8,13 @@ from qutebrowser.misc import objects
 from os import path
 import tempfile
 import subprocess
+import threading
 import json
 
 @cmdutils.register(instance='command-dispatcher', scope='window', overwrite=True)
 @cmdutils.argument('close', choices=['True', 'False'])
 @cmdutils.argument('prefix')
-def shell_send(self, close, prefix='', quiet=False):
+def shell_send(self, close=False, prefix='', quiet=False):
 	''' Executes program with current selection as stdin (may be none)'''
 	maybeshell = '' if close else ' && $SHELL'
 
@@ -22,14 +23,20 @@ def shell_send(self, close, prefix='', quiet=False):
 			if not text and not quiet:
 				message.info("Nothing selected")
 
-			tempname = None
-			with tempfile.TemporaryFile(mode='wt', encoding='utf-8') as infile:
-				infile.write(text)
-				tempname = infile.name
+			def inthread():
+				with tempfile.NamedTemporaryFile(mode='wt', encoding='utf-8') as infile:
+					infile.write(text)
+					infile.flush()
 
-			retcode = subprocess.call(f"x-terminal-emulator-exe {prefix} 'cat {tempname} | {program} {maybeshell}'")
-			if retcode != 0:
-				message.error(f'Shell subprocess failed with: {retcode}')
+					tempname = '"' + infile.name + '"'
+					cmd = f"x-terminal-emulator-exe {prefix} 'cat {tempname} | {program} {maybeshell}'"
+					p = subprocess.Popen(['sh', '-c', cmd])
+					p.wait()
+
+					if p.returncode != 0 and not quiet:
+						message.error(f'shell-send process "{cmd}" failed with: {p.returncode}')
+
+			threading.Thread(target=inthread).start()
 
 		caret = self._current_widget().caret
 		caret.selection(callback=_selection_callback)
